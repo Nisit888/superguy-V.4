302b464c15d844d77239e83a968d8e7c0ac1f315
Nisit888 <kikimukuro@gmail.com>
2017-07-30 13:44:38 +0700

    Notification

--- app/src/main/java/com/mycom/coe/myfragment/MainActivity.java
+++ app/src/main/java/com/mycom/coe/myfragment/MainActivity.java
@@ -3,6 +3,7 @@ package com.mycom.coe.myfragment;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnThree;
     private Button btnBack;
     private Button btnNotification;
+    private Button btnCancel;
 
 
     @Override
@@ -38,17 +40,11 @@ public class MainActivity extends AppCompatActivity {
         startMultipleFragment();
 
         btnNotification = (Button) findViewById(R.id.btnNotification);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
 
         btnNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NotificationCompat.Builder mBuilder =
-                        (NotificationCompat.Builder) new NotificationCompat
-                                .Builder(MainActivity.this)
-                                .setSmallIcon(R.drawable.ic_stat_name)
-                                .setContentTitle("My notification")
-                                .setContentText("Open Google website!");
-
                 Intent resultIntent = new Intent(Intent.ACTION_VIEW,
                         Uri.parse("http://www.google.com/"));
 
@@ -61,7 +57,24 @@ public class MainActivity extends AppCompatActivity {
                                 // if a previous PendingIntent already exists,
                                 // then the current one will update it with the latest intent
                                 PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+                NotificationCompat.Builder mBuilder =
+                        (NotificationCompat.Builder) new NotificationCompat
+                                .Builder(MainActivity.this)
+                                .setSmallIcon(R.drawable.ic_stat_name)
+                                .setContentTitle("My notification")
+                                .setContentText("Open Google website!")
+
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setVibrate(new long[]{1000})
+
+                                .addAction(0, "Action", resultPendingIntent)
+                                .addAction(0, "Close", null)
+                                .setAutoCancel(true);
+
                 mBuilder.setContentIntent(resultPendingIntent);
+
 
                 NotificationManager mNotifyMgr =
                         (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
@@ -71,101 +84,111 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ns = Context.NOTIFICATION_SERVICE;
+                NotificationManager nMgr = (NotificationManager) getApplicationContext().getSystemService(ns);
+                nMgr.cancel(NOTIFICATION_ID);
+            }
+        });
+
+
     }
 
-private void startMultipleFragment(){
-        btnOne=(Button)findViewById(R.id.btnOne);
-        btnTwo=(Button)findViewById(R.id.btnTwo);
-        btnThree=(Button)findViewById(R.id.btnThree);
-        btnBack=(Button)findViewById(R.id.btnBack);
+    private void startMultipleFragment() {
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnBack = (Button) findViewById(R.id.btnBack);
 
-        btnBack.setOnClickListener(new View.OnClickListener(){
-@Override
-public void onClick(View view){
-        Fragment fragment=getSupportFragmentManager().findFragmentByTag(TAG);
-        if(fragment!=null){
-        getSupportFragmentManager().popBackStack();
-        }
-        }
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = getSupportFragmentManager().findFragmentByTag(TAG);
+                if (fragment != null) {
+                    getSupportFragmentManager().popBackStack();
+                }
+            }
         });
 
-        btnOne.setOnClickListener(new View.OnClickListener(){
-@Override
-public void onClick(View view){
-        getSupportFragmentManager()
-        .beginTransaction()
-        .replace(R.id.javaFragment,new OneFragment(),TAG)
-        .addToBackStack(null)
-        .commit();
-        }
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.javaFragment, new OneFragment(), TAG)
+                        .addToBackStack(null)
+                        .commit();
+            }
         });
-        btnTwo.setOnClickListener(new View.OnClickListener(){
-@Override
-public void onClick(View view){
-        getSupportFragmentManager()
-        .beginTransaction()
-        .replace(R.id.javaFragment,new TwoFragment(),TAG)
-        .addToBackStack(null)
-        .commit();
-        }
+        btnTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.javaFragment, new TwoFragment(), TAG)
+                        .addToBackStack(null)
+                        .commit();
+            }
         });
 
-        btnThree.setOnClickListener(new View.OnClickListener(){
-@Override
-public void onClick(View view){
-        getSupportFragmentManager()
-        .beginTransaction()
-        .replace(R.id.javaFragment,ThreeFragment.newInstance("Three F"),TAG)
-        .addToBackStack(null)
-        .commit();
-        }
+        btnThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.javaFragment, ThreeFragment.newInstance("Three F"), TAG)
+                        .addToBackStack(null)
+                        .commit();
+            }
         });
-        }
+    }
 
-private void addRemoveFragment(){
-        btnAdd=(Button)findViewById(R.id.btnAdd);
-        btnRemove=(Button)findViewById(R.id.btnRemove);
+    private void addRemoveFragment() {
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnRemove = (Button) findViewById(R.id.btnRemove);
 
-        btnAdd.setOnClickListener(new View.OnClickListener(){
-@Override
-public void onClick(View view){
-        JavaFragment javaFragment=new JavaFragment();
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction transaction=fragmentManager.beginTransaction();
-        transaction.replace(R.id.javaFragment,
-        javaFragment,
-        TAG);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JavaFragment javaFragment = new JavaFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.javaFragment,
+                        javaFragment,
+                        TAG);
+                transaction.commit();
+            }
+        });
+
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = getSupportFragmentManager()
+                        .findFragmentByTag(TAG);
+                if (fragment != null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .remove(fragment)
+                            .commit();
+                }
+            }
+        });
+    }
+
+    private void startJavaFragment() {
+        JavaFragment javaFragment = new JavaFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.javaFragment, javaFragment);
         transaction.commit();
-        }
-        });
+    }
 
-        btnRemove.setOnClickListener(new View.OnClickListener(){
-@Override
-public void onClick(View view){
-        Fragment fragment=getSupportFragmentManager()
-        .findFragmentByTag(TAG);
-        if(fragment!=null){
-        getSupportFragmentManager()
-        .beginTransaction()
-        .remove(fragment)
-        .commit();
-        }
-        }
-        });
-        }
-
-private void startJavaFragment(){
-        JavaFragment javaFragment=new JavaFragment();
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction transaction=fragmentManager.beginTransaction();
-        transaction.replace(R.id.javaFragment,javaFragment);
-        transaction.commit();
-        }
-
-private void startBlankFragment(){
-        BlankFragment blankFragment=(BlankFragment)getSupportFragmentManager()
-        .findFragmentById(R.id.fragment);
-        TextView tvf1=(TextView)blankFragment.getView().findViewById(R.id.tvf1);
+    private void startBlankFragment() {
+        BlankFragment blankFragment = (BlankFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment);
+        TextView tvf1 = (TextView) blankFragment.getView().findViewById(R.id.tvf1);
         tvf1.setText("Blank fragment");
-        }
-        }
+    }
+}
